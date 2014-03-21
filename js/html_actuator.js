50f2ce848632ace762697c5efab9afe7913f6e16
Jerad Parish <jparish@minutebids.com>
2014-03-20 21:34:20 -0500

    fix merge conflicts

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -39,15 +39,7 @@ HTMLActuator.prototype.actuate = function (grid, metadata) {
 };
 
 // Continues the game (both restart and keep playing)
-<<<<<<< HEAD
-HTMLActuator.prototype.continue = function () {
-  if (typeof ga !== "undefined") {
-    ga("send", "event", "game", "restart");
-  }
-
-=======
 HTMLActuator.prototype.continue = function (keepPlaying) {
->>>>>>> master
   this.clearMessage();
   if (keepPlaying) {
     $("#options-container").html("Restart the game to enable autoplay and tile placement modes!");
@@ -142,16 +134,10 @@ HTMLActuator.prototype.message = function (won) {
   var type    = won ? "game-won" : "game-over";
   var message = won ? "You win!" : "Game over!";
 
-  if (typeof ga !== "undefined") {
-    ga("send", "event", "game", "end", type, this.score);
-  }
-
   this.messageContainer.classList.add(type);
   this.messageContainer.getElementsByTagName("p")[0].textContent = message;
 
   this.clearContainer(this.sharingContainer);
-  this.sharingContainer.appendChild(this.scoreTweetButton());
-  twttr.widgets.load();
 };
 
 HTMLActuator.prototype.clearMessage = function () {
@@ -160,28 +146,9 @@ HTMLActuator.prototype.clearMessage = function () {
   this.messageContainer.classList.remove("game-over");
 };
 
-<<<<<<< HEAD
-HTMLActuator.prototype.scoreTweetButton = function () {
-  var tweet = document.createElement("a");
-  tweet.classList.add("twitter-share-button");
-  tweet.setAttribute("href", "https://twitter.com/share");
-  tweet.setAttribute("data-via", "gabrielecirulli");
-  tweet.setAttribute("data-url", "http://git.io/2048");
-  tweet.setAttribute("data-counturl", "http://gabrielecirulli.github.io/2048/");
-  tweet.textContent = "Tweet";
-
-  var text = "I scored " + this.score + " points at 2048, a game where you " +
-             "join numbers to score high! #2048game";
-  tweet.setAttribute("data-text", text);
-
-  return tweet;
-};
-=======
 HTMLActuator.prototype.aiStatsMode = function(on) {
   if (on) {
     this.aiStatsContainer.find("#ai-stats-none").hide();
     this.aiStatsContainer.find("#ai-stats").show();
   }
 };
-
->>>>>>> master
